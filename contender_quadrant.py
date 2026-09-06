86d1e29115433f4c36598241a7861b240a3b267a
sais2311 <138555000+sais2311@users.noreply.github.com>
2026-09-06 14:55:28 +1000

    Fix typo in comment about premiership zone
    Corrected typo in comment regarding premiership zone criteria.

--- contender_quadrant.py
+++ contender_quadrant.py
@@ -35,7 +35,7 @@ fig, ax = plt.subplots(figsize=(11, 8))
 ax.set_xlim(x_min, x_max)
 ax.set_ylim(y_max, y_min)   
 
-# Making the premiership zone: placing teams which havea a better-than-average attack AND defence
+# Making the premiership zone: placing teams which havea a better than average attack AND defence
 ax.add_patch(Rectangle((avg_attack, avg_defence), x_max - avg_attack, y_min - avg_defence,
                        color="green", alpha=0.08, zorder=0))
 
